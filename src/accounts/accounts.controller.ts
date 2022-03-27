38962ce4f8d55661f2431acf3ba185da614dbb02
Junsu Min <sunsuking@gmail.com>
2022-03-27 21:36:20 +0900

    accounts API 수정 - Better Comment 를 위한 처리 - API 명세 작성 - Review 관련 추가

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -8,21 +8,31 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AccountsService } from 'accounts/accounts.service';
-import { Address, Bucket, Image, Profile, User } from '@prisma/client';
-import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Address, Bucket, Image, Profile, Review, User } from '@prisma/client';
+import { UpdateProfileDto } from 'accounts/dto/update-profile.dto';
 import { JWTAuthGuard } from 'auth/auth.guard';
 import { GetUser } from 'auth/get-user-decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('/v1/accounts')
+@ApiTags('Accounts API')
+@Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
 
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 정보 조회',
+    description: 'JWT 토큰으로 로그인된 사용자의 Profile 조회한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Get('/me')
   async findProfile(@GetUser() user: User): Promise<Profile> {
     return this.accountsService.findProfileByUser(user);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 정보 수정',
+    description: 'JWT 토큰으로 로그인된 사용자의 Profile 변경하는데 사용한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Patch('/me/update')
   async updateProfile(
@@ -32,18 +42,31 @@ export class AccountsController {
     return this.accountsService.updateProfile(user, updateProfileDto);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 배송지 정보들 조회',
+    description: 'JWT 토큰으로 로그인된 사용자의 배송지 정보들을 조회한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Get('/addresses')
   async findProfileAddresses(@GetUser() user: User): Promise<Address[]> {
     return this.accountsService.findProfileAddresses(user);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 업로드 이미지들 조회',
+    description:
+      'JWT 토큰으로 로그인된 사용자의 업로드 이미지 조회들 조회한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Get('/images')
   async findProfileImages(@GetUser() user: User): Promise<Image[]> {
     return this.accountsService.findProfileImages(user);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 업로드 이미지 삭제',
+    description: 'JWT 토큰으로 로그인된 사용자의 업로드 이미지를 삭제한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Delete('/image/:id')
   async deleteImage(@GetUser() user: User, id: number): Promise<void> {
@@ -53,12 +76,55 @@ export class AccountsController {
     return this.accountsService.deleteImage(id);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 작성 리뷰들 조회',
+    description: 'JWT 토큰으로 로그인된 사용자가 작성한 리뷰들을 조회한다.',
+  })
+  @UseGuards(JWTAuthGuard)
+  @Get('/reviews')
+  async findMyReviewsByUser(@GetUser() user: User): Promise<Review[]> {
+    return this.accountsService.findMyReviewsByUser(user);
+  }
+
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 스크랩 리뷰들 조회',
+    description: 'JWT 토큰으로 로그인된 사용자가 스크랩한 리뷰들을 조회한다.',
+  })
+  @UseGuards(JWTAuthGuard)
+  @Get('/reviews/scrapped')
+  async findScrappedReviewsByUser(
+    @GetUser() user: User,
+  ): Promise<{ scrapedReviews: Review[] }> {
+    return this.accountsService.findScrappedReviewsByUser(user);
+  }
+
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 좋아요 리뷰들 조회',
+    description: 'JWT 토큰으로 로그인된 사용자가 좋아요한 리뷰들을 조회한다.',
+  })
+  @UseGuards(JWTAuthGuard)
+  @Get('/reviews/liked')
+  async findLikedReviewsByUser(
+    @GetUser() user: User,
+  ): Promise<{ likedReviews: Review[] }> {
+    return this.accountsService.findLikedReviewsByUser(user);
+  }
+
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 위시리스트 조회',
+    description: 'JWT 토큰으로 로그인된 사용자의 위시리스트 조회한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Get('/buckets')
   async findProfileBuckets(@GetUser() user: User): Promise<Bucket[]> {
     return this.accountsService.findProfileBucketLists(user);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 위시리스트 변경(장바구니로)',
+    description:
+      'JWT 토큰으로 로그인된 사용자의 위시리스트 물품을 장바구니로 이동한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Put('/bucket/:id')
   async changeBucketStatus(@GetUser() user: User, id: number): Promise<Bucket> {
@@ -68,6 +134,10 @@ export class AccountsController {
     return this.accountsService.changeBucketStatus(id);
   }
 
+  @ApiOperation({
+    summary: '로그인된 사용자 계정 장바구니 조회',
+    description: 'JWT 토큰으로 로그인된 사용자의 장바구니를 조회한다.',
+  })
   @UseGuards(JWTAuthGuard)
   @Get('/shops')
   async findShops(@GetUser() user: User): Promise<Bucket[]> {
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma.service';
-import { Address, Bucket, Image, Profile, User } from '@prisma/client';
+import { Address, Bucket, Image, Profile, Review, User } from '@prisma/client';
 
 import { CreateProfileDto } from 'accounts/dto/create-profile.dto';
 import { UpdateProfileDto } from 'accounts/dto/update-profile.dto';
@@ -9,7 +9,7 @@ import { UpdateProfileDto } from 'accounts/dto/update-profile.dto';
 export class AccountsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // 프로필 생성을 위하여 사용 -> API 제외, User 와 연동해서 사용
+  // * 프로필 생성을 위하여 사용 -> API 제외, User 와 연동해서 사용
   async createProfile(createProfileDto: CreateProfileDto): Promise<Profile> {
     return this.prisma.profile.create({
       data: createProfileDto,
@@ -22,7 +22,7 @@ export class AccountsService {
     });
   }
 
-  // 프로필을 수정하기 위하여 사용
+  // * 프로필을 수정하기 위하여 사용
   async updateProfile(
     user: User,
     updateProfileDto: UpdateProfileDto,
@@ -34,14 +34,14 @@ export class AccountsService {
     });
   }
 
-  // 프로필을 삭제하기 위하여 사용 -> API 제외, User 와 연동해서 사용
+  // * 프로필을 삭제하기 위하여 사용 -> API 제외, User 와 연동해서 사용
   async deleteProfile(id: number): Promise<void> {
     this.prisma.profile.delete({
       where: { id },
     });
   }
 
-  // 프로필 포인트 정산을 위하여 사용 -> API 제외, User 구매 시 사용
+  // * 프로필 포인트 정산을 위하여 사용 -> API 제외, User 구매 시 사용
   async calculateProfilePoint(user: User): Promise<void> {
     const profile: Profile = await this.findProfileByUser(user);
     this.prisma.point.findMany({
@@ -49,7 +49,7 @@ export class AccountsService {
     });
   }
 
-  // 프로필에 해당하는 배송지들을 우선순위를 기준으로 조회하기 위하여 사용
+  // * 프로필에 해당하는 배송지들을 우선순위를 기준으로 조회하기 위하여 사용
   async findProfileAddresses(user: User): Promise<Address[]> {
     const profile: Profile = await this.findProfileByUser(user);
     return this.prisma.address.findMany({
@@ -65,7 +65,7 @@ export class AccountsService {
     });
   }
 
-  // 프로필에 해당하는 업로드 이미지들을 업로드일을 기준으로 조회하기 위하여 사용
+  // * 프로필에 해당하는 업로드 이미지들을 업로드일을 기준으로 조회하기 위하여 사용
   async findProfileImages(user: User): Promise<Image[]> {
     const profile: Profile = await this.findProfileByUser(user);
     return this.prisma.image.findMany({
@@ -74,20 +74,58 @@ export class AccountsService {
     });
   }
 
-  // 특정한 이미지를 삭제하기 위하여 사용
+  // * 특정한 이미지를 삭제하기 위하여 사용
   async deleteImage(imageId: number): Promise<void> {
     this.prisma.image.delete({
       where: { id: imageId },
     });
   }
 
+  // * 특정한 이미지가 사용자의 것인지 확인하기 위하여 사용
   async isImageOwner(id: number, user: User): Promise<boolean> {
     const image: Image = await this.findImageById(id);
     const profile: Profile = await this.findProfileByUser(user);
     return image.profileId === profile.id;
   }
 
-  // 프로필에 해당하는 버킷리스트들을 조회하기 위하여 사용
+  // * 사용자가 작성한 리뷰들을 확인하기 위하여 사용
+  async findMyReviewsByUser(user: User): Promise<Review[]> {
+    const profile: Profile = await this.findProfileByUser(user);
+    return this.prisma.review.findMany({
+      where: { profileId: profile.id },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  // * 사용자가 스크랩한 리뷰들을 확인하기 위하여 사용
+  async findScrappedReviewsByUser(
+    user: User,
+  ): Promise<{ scrapedReviews: Review[] }> {
+    return this.prisma.profile.findUnique({
+      where: { userId: user.id },
+      select: {
+        scrapedReviews: {
+          include: { profile: true },
+        },
+      },
+    });
+  }
+
+  // * 사용자가 좋아요한 리뷰들을 확인하기 위하여 사용
+  async findLikedReviewsByUser(
+    user: User,
+  ): Promise<{ likedReviews: Review[] }> {
+    return this.prisma.profile.findUnique({
+      where: { userId: user.id },
+      select: {
+        likedReviews: {
+          include: { profile: true },
+        },
+      },
+    });
+  }
+
+  // * 프로필에 해당하는 버킷리스트들을 조회하기 위하여 사용
   async findProfileBucketLists(user: User): Promise<Bucket[]> {
     const profile: Profile = await this.findProfileByUser(user);
     return this.prisma.bucket.findMany({
@@ -96,14 +134,14 @@ export class AccountsService {
     });
   }
 
-  // 버킷의 현재 상태를 조회하기 위하여 사용 (상태 변경을 편하게 하기 위하여)
+  // * 버킷의 현재 상태를 조회하기 위하여 사용 (상태 변경을 편하게 하기 위하여)
   private async findBucket(id: number): Promise<Bucket> {
     return this.prisma.bucket.findUnique({
       where: { id },
     });
   }
 
-  // 버킷리스트의 상태를 변경하기 위하여 사용
+  // * 버킷리스트의 상태를 변경하기 위하여 사용
   async changeBucketStatus(id: number): Promise<Bucket> {
     const bucket: Bucket = await this.findBucket(id);
     return this.prisma.bucket.update({
@@ -120,7 +158,7 @@ export class AccountsService {
     return bucket.profileId === profile.id;
   }
 
-  // 프로필에 해당하는 장바구니 상품들을 조회하기 위하여 사용
+  // * 프로필에 해당하는 장바구니 상품들을 조회하기 위하여 사용
   async findProfileShoppingLists(user: User): Promise<Bucket[]> {
     const profile: Profile = await this.findProfileByUser(user);
     return this.prisma.bucket.findMany({
